d63a079227eebe7b9a5bb1e9dd9469a75bd07639
Yurie <yurie.koga.a@gmail.com>
2019-12-27 19:42:03 -0800

    create app

---
+++ text-flex/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Flex from "./components/Flex";
+
+function App() {
+  return (
+    <div>
+      <Flex />
+    </div>
+  );
+}
+
+export default App;
---
+++ text-flex/src/components/Flex.js
@@ -0,0 +1,146 @@
+import React from "react";
+
+export default function Flex() {
+  return (
+    <div>
+      <h4>aaa</h4>
+      <div className="fd-r">
+        <div className="f-item inside-item">1</div>
+        <div className="f-item inside-item">2</div>
+        <div className="f-item inside-item">3</div>
+        <div className="f-item inside-item">4</div>
+        <div className="f-item inside-item">5</div>
+      </div>
+
+      <br />
+
+      <div className="fd-rr">
+        <div className="f-item inside-item">1</div>
+        <div className="f-item inside-item">2</div>
+        <div className="f-item inside-item">3</div>
+        <div className="f-item inside-item">4</div>
+        <div className="f-item inside-item">5</div>
+      </div>
+
+      <br />
+
+      <div className="fd-c">
+        <div className="f-item inside-item">1</div>
+        <div className="f-item inside-item">2</div>
+        <div className="f-item inside-item">3</div>
+        <div className="f-item inside-item">4</div>
+        <div className="f-item inside-item">5</div>
+      </div>
+
+      <br />
+
+      <div className="fd-cr">
+        <div className="f-item inside-item">1</div>
+        <div className="f-item inside-item">2</div>
+        <div className="f-item inside-item">3</div>
+        <div className="f-item inside-item">4</div>
+        <div className="f-item inside-item">5</div>
+      </div>
+
+      <br />
+
+      <div className="fw-nw">
+        <div className="f-item inside-item">1</div>
+        <div className="f-item inside-item">2</div>
+        <div className="f-item inside-item">3</div>
+        <div className="f-item inside-item">4</div>
+        <div className="f-item inside-item">5</div>
+      </div>
+
+      <br />
+
+      <div className="fw-w">
+        <div className="f-item inside-item">1</div>
+        <div className="f-item inside-item">2</div>
+        <div className="f-item inside-item">3</div>
+        <div className="f-item inside-item">4</div>
+        <div className="f-item inside-item">5</div>
+      </div>
+
+      <br />
+
+      <div className="fw-wr">
+        <div className="f-item inside-item">1</div>
+        <div className="f-item inside-item">2</div>
+        <div className="f-item inside-item">3</div>
+        <div className="f-item inside-item">4</div>
+        <div className="f-item inside-item">5</div>
+      </div>
+
+      <br />
+
+      <div className="ff-rw">
+        <div className="f-item inside-item">1</div>
+        <div className="f-item inside-item">2</div>
+        <div className="f-item inside-item">3</div>
+        <div className="f-item inside-item">4</div>
+        <div className="f-item inside-item">5</div>
+      </div>
+
+      <br />
+
+      <div className="jc-fs">
+        <div className="f-item inside-item">1</div>
+        <div className="f-item inside-item">2</div>
+        <div className="f-item inside-item">3</div>
+        <div className="f-item inside-item">4</div>
+        <div className="f-item inside-item">5</div>
+      </div>
+
+      <br />
+
+      <div className="jc-fe">
+        <div className="f-item inside-item">1</div>
+        <div className="f-item inside-item">2</div>
+        <div className="f-item inside-item">3</div>
+        <div className="f-item inside-item">4</div>
+        <div className="f-item inside-item">5</div>
+      </div>
+
+      <br />
+
+      <div className="jc-c">
+        <div className="f-item inside-item">1</div>
+        <div className="f-item inside-item">2</div>
+        <div className="f-item inside-item">3</div>
+        <div className="f-item inside-item">4</div>
+        <div className="f-item inside-item">5</div>
+      </div>
+
+      <br />
+
+      <div className="jc-sb">
+        <div className="f-item inside-item">1</div>
+        <div className="f-item inside-item">2</div>
+        <div className="f-item inside-item">3</div>
+        <div className="f-item inside-item">4</div>
+        <div className="f-item inside-item">5</div>
+      </div>
+
+      <br />
+
+      <div className="jc-se">
+        <div className="f-item inside-item">1</div>
+        <div className="f-item inside-item">2</div>
+        <div className="f-item inside-item">3</div>
+        <div className="f-item inside-item">4</div>
+        <div className="f-item inside-item">5</div>
+      </div>
+
+      <br />
+
+      <div className="jc-sa">
+        <div className="f-item inside-item">1</div>
+        <div className="f-item inside-item">2</div>
+        <div className="f-item inside-item">3</div>
+        <div className="f-item inside-item">4</div>
+        <div className="f-item inside-item">5</div>
+      </div>
+    </div>
+  );
+}
